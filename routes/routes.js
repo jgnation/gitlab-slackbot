d3466b861cff444bcd67aa95ca03191b941bded3
Jeremy Gustine <jgustine@homeadvisor.com>
2016-06-20 14:27:26 -0600

    Add ability to send gitlab token as query parameter.

--- routes/routes.js
+++ routes/routes.js
@@ -19,15 +19,22 @@ router.post('/slack-message', function(req, res) {
 router.post('/gitlab-hook', function(req, res) {
 	//TODO validate the request
 
-	var token = req.get('x-gitlab-token');
-
-	if(token) {
-		if (token == process.env.GITLAB_WEBHOOK_TOKEN) {
+	var header_token = req.get('x-gitlab-token');
+	var query_token = req.query.token;
+	
+	if(header_token) {
+		if (header_token == process.env.GITLAB_WEBHOOK_TOKEN) {
+			gitlabHandler(req, res);
+			return;
+		}
+	} else if (query_token) {
+		if (query_token == process.env.GITLAB_WEBHOOK_TOKEN) {
 			gitlabHandler(req, res);
 			return;
 		}
 	}
 	res.sendStatus(401);
+		
 });
 
 module.exports = router;
