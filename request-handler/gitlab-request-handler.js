a6b4066f700f364b35d0ace0b2645019bf9952a7
Jeremy Gustine <jgustine@homeadvisor.com>
2016-07-29 13:28:18 -0600

    Adding logging.

--- request-handler/gitlab-request-handler.js
+++ request-handler/gitlab-request-handler.js
@@ -2,28 +2,34 @@ var queue = require('../amqp/amqp')
 
 module.exports = function handleRequest(req, res) {
 	var hook = req.body;
-	console.log(req.body);
-	if (hook.object_kind != 'push') {
+
+	if (hook.object_kind == 'push') {
+		var commits = hook.commits;
+		var commit_messages = commits.map( commit => commit.message );
+
+		var ref = hook.ref;
+		var ref_components = ref.split("/");
+		var branch = ref_components[ref_components.length - 1];
+
+		var message = {
+			repo: hook.repository.name,
+			commit_messages: commit_messages,
+			user_name: hook.user_name,
+			branch: branch
+		}
+
+		queue.publishMessage(message); //TODO: handle error
+	} else if (hook.object_king == 'merge_request') {
+		console.log('!!!!!!!!!!!')
+		var object_attributes = hook.object_attributes;
+		var description = object_attributes.description;
+		console.log('description = ' + description);
+		var repository = hook.repository.name;
+		console.log('repository = ' + repository);
+	} else {
 		//ignore this request;
 		res.sendStatus(200);
 		return;
 	}
-
-	var commits = hook.commits;
-	var commit_messages = commits.map( commit => commit.message );
-
-	var ref = hook.ref;
-	var ref_components = ref.split("/");
-	var branch = ref_components[ref_components.length - 1];
-
-	var message = {
-		repo: hook.repository.name,
-		commit_messages: commit_messages,
-		user_name: hook.user_name,
-		branch: branch
-	}
-
-	queue.publishMessage(message); //TODO: handle error
-
 	res.sendStatus(200);
 };
