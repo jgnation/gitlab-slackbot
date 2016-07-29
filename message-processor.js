690c37374363fa9312fde413b59eeb8d4386f8d8
Jeremy Gustine <jgustine@homeadvisor.com>
2016-07-29 13:13:30 -0600

    Add merge request notification support.

--- message-processor.js
+++ message-processor.js
@@ -26,58 +26,126 @@ connection.on('ready', function () {
 
         // Receive messages
         q.subscribe(function (message) {
-            var repo = message.body.repo;
-            var commit_messages = message.body.commit_messages;
-            var num_commits = commit_messages.length;
-            var user_name = message.body.user_name;
-            var branch = message.body.branch;
+            var type = message.body.type;
 
-            redis_client.smembersAsync(repo)
-                .then(function (results) {
-                    var fields = [];
-                    var fallback_msg = user_name + " pushed " + num_commits + " commits to repo: " + repo + ", branch: " + branch;
-                    for (var j = 0; j < commit_messages.length; j++) {
-                        fallback_msg += "\n- " + commit_messages[j];
-                        var field = {
-                            title: '',
-                            value: "`" + commit_messages[j].trim() + "`", //trim is required to remove the /n character.  I'm not sure why that character is present.
-                            short: false
+            //TOOD: get rid of these if/switch statements
+            if (type == 'push') {
+                var repo = message.body.repo;
+                var commit_messages = message.body.commit_messages;
+                var num_commits = commit_messages.length;
+                var user_name = message.body.user_name;
+                var branch = message.body.branch;
+
+                redis_client.smembersAsync(repo)
+                    .then(function (results) {
+                        var fields = [];
+                        var fallback_msg = user_name + " pushed " + num_commits + " commits to repo: " + repo + ", branch: " + branch;
+                        for (var j = 0; j < commit_messages.length; j++) {
+                            fallback_msg += "\n- " + commit_messages[j];
+                            var field = {
+                                title: '',
+                                value: "`" + commit_messages[j].trim() + "`", //trim is required to remove the /n character.  I'm not sure why that character is present.
+                                short: false
+                            }
+
+                            fields.push(field)
                         }
 
-                        fields.push(field)
-                    }
+                        for (var i = 0; i < results.length; i++) {
+                            var requestBody = {
+                                channel: results[i],
+                                text: '',
+                                attachments: [
+                                    {
+                                        fallback: fallback_msg,
+                                        color: "#36a64f",
+                                        pretext: "*" + user_name + "* pushed " + num_commits + " commits to repo: *" + repo + "*, branch: *" + branch + "*",
+                                        mrkdwn_in: [ "fields", "pretext" ],
+                                        fields: fields
+                                    }
+                                ]
+                            };
 
-                    for (var i = 0; i < results.length; i++) {
-                        var requestBody = {
-                            channel: results[i],
-                            text: '',
-                            attachments: [
-                                {
-                                    fallback: fallback_msg,
-                                    color: "#36a64f",
-                                    pretext: "*" + user_name + "* pushed " + num_commits + " commits to repo: *" + repo + "*, branch: *" + branch + "*",
-                                    mrkdwn_in: [ "fields", "pretext" ],
-                                    fields: fields
+                            var requestObject = {
+                                    url: process.env.SLACK_PUSH_HOOK_URL,
+                                    method: 'POST',
+                                    json: requestBody
+                            };
+
+                            request(requestObject, function(error, response, body) {
+                                if(error) {
+                                    console.log("Error while POSTing to slack: " + error);
                                 }
-                            ]
-                        };
+                            });
+                        }
+                    })
+                    .catch(function(error) {
+                        console.log("Error while retrieving data from redis: " + error); 
+                    });
+            }
 
-                        var requestObject = {
-                                url: process.env.SLACK_PUSH_HOOK_URL,
-                                method: 'POST',
-                                json: requestBody
-                        };
+            if (type == 'merge_request') {
+                var repo = message.body.repo;
+                var title = message.body.title;
+                var description = message.body.description;
+                var user = message.body.user;
+                var assignee = message.body.assignee;
+                var url = message.body.url;
+                var state = message.body.state;
 
-                        request(requestObject, function(error, response, body) {
-                            if(error) {
-                                console.log("Error while POSTing to slack: " + error);
-                            }
-                        });
-                    }
-                })
-                .catch(function(error) {
-                    console.log("Error while retrieving data from redis: " + error); 
-                });
+                redis_client.smembersAsync(repo)
+                    .then(function (results) {
+                        for (var i = 0; i < results.length; i++) {
+
+                            var fields = [];
+
+                            var message = "*Repo:* " + repo + "\n";
+                            message += "*State:* " + state + "\n";
+                            message += "*User:* " + user + "\n";
+                            if (assignee) message += "*Assigned to:* " + assignee + "\n";
+                            message += "*Title:* " + title + "\n";
+                            if (description) message += "*Description:* " + description + "\n";
+
+                            fields.push({
+                                title: '',
+                                value: message,
+                                short: false
+                            });
+
+                            var requestBody = {
+                                channel: results[i],
+                                text: '',
+                                attachments: [
+                                    {
+                                        title: 'Merge Request Notification',
+                                        fallback: 'New merge request: ' + url,
+                                        color: "#ff9900",
+                                        mrkdwn_in: [ "fields"],
+                                        fields: fields,
+                                        footer: url
+                                    }
+                                ]                             
+                            };
+
+                            var requestObject = {
+                                    url: process.env.SLACK_PUSH_HOOK_URL,
+                                    method: 'POST',
+                                    json: requestBody
+                            };
+
+                            request(requestObject, function(error, response, body) {
+                                if(error) {
+                                    console.log("Error while POSTing to slack: " + error);
+                                }
+                            });
+                        }
+                    })
+                    .catch(function(error) {
+                        console.log("Error while retrieving data from redis: " + error); 
+                    });
+
+            }
+            
         });
     });
 });
--- request-handler/gitlab-request-handler.js
+++ request-handler/gitlab-request-handler.js
@@ -2,27 +2,41 @@ var queue = require('../amqp/amqp')
 
 module.exports = function handleRequest(req, res) {
 	var hook = req.body;
-	if (hook.object_kind != 'push') {
+
+	//TODO: get rid of if/switch statements
+	if (hook.object_kind == 'push') {
+		var commits = hook.commits;
+		var commit_messages = commits.map( commit => commit.message );
+
+		var ref = hook.ref;
+		var ref_components = ref.split("/");
+		var branch = ref_components[ref_components.length - 1];
+
+		var message = {
+			type: 'push',
+			repo: hook.repository.name,
+			commit_messages: commit_messages,
+			user_name: hook.user_name,
+			branch: branch
+		}
+
+		queue.publishMessage(message); //TODO: handle error
+	} else if (hook.object_kind == 'merge_request') {
+		var message = {
+			type: 'merge_request',
+			repo: hook.repository.name,
+			title: hook.object_attributes.title,
+			description: hook.object_attributes.description,
+			user: hook.user.username,
+			assignee: hook.assignee.username,
+			url: hook.object_attributes.url,
+			state: hook.object_attributes.state
+		};
+		queue.publishMessage(message); //TODO: handle error
+	} else {
 		//ignore this request;
 		res.sendStatus(200);
 		return;
 	}
-
-	var commits = hook.commits;
-	var commit_messages = commits.map( commit => commit.message );
-
-	var ref = hook.ref;
-	var ref_components = ref.split("/");
-	var branch = ref_components[ref_components.length - 1];
-
-	var message = {
-		repo: hook.repository.name,
-		commit_messages: commit_messages,
-		user_name: hook.user_name,
-		branch: branch
-	}
-
-	queue.publishMessage(message); //TODO: handle error
-
 	res.sendStatus(200);
 };
